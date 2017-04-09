2a7402c869c5c7e9d98722fae36c727e3697a873
barriquello <barriquello@gmail.com>
2017-04-09 08:42:23 -0300

    adiciona esp8266 AT driver

---
+++ brtos_unet/src/dev/esp8266/esp8266_at.c
@@ -0,0 +1,242 @@
+/* The License
+ * 
+ * Copyright (c) 2015 Universidade Federal de Santa Maria
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+
+*/
+/*
+ * esp8266_at.c
+ *
+ *  Created on: Mar 17, 2015
+ *      Author: Carlos H. Barriquello
+ */
+
+
+#include "esp8266_at.h"
+#include "BRTOS.h"
+#include "terminal_io.h"
+#include "terminal_commands.h"
+#include "virtual_com.h"
+#include "utils.h"
+
+static esp_state_t esp_state = INIT;
+static char ip[16];
+static char* hostname = NULL;
+
+static void at_esp_print_reply(void)
+{
+	INT8U c;
+	DelayTask(1000);
+	while((c=at_esp_getchar()) != (CHAR8)-1){
+		putcharSer(USE_USB,c);
+	}
+}
+
+static INT8U at_esp_get_reply(INT8U *buf, INT8U max_len)
+{
+	INT8U c;
+	INT8U len = 0;
+	DelayTask(1000);
+	while((c=at_esp_getchar()) != (CHAR8)-1){
+		*buf = c;
+		buf++;
+		len++;
+		max_len--;
+		if(max_len == 0) return len;
+	}
+	return len;
+}
+
+INT8U esp_set_hostname(CHAR8 *host)
+{
+	hostname = host;
+	return OK;
+}
+
+INT8U esp_get_ip(void)
+{
+	if(hostname == NULL)
+	{
+		return !OK;
+	}
+	
+	esp_acquire();	
+	esp_print("AT+CIPRESOLVE=\"");
+	esp_print(hostname);
+	esp_print("\"\r\n");
+	at_esp_get_reply(ip,16);
+	//at_esp_print_reply();
+	esp_release();
+	
+	return OK;
+	
+}
+
+CHAR8 at_esp_getchar(void)
+{
+	INT8U caracter;
+	if(OSQueuePend(ESP_QUEUE, &caracter, ESP_UART_TIMEOUT) == TIMEOUT)
+	{
+		return (CHAR8) (-1);
+	}	
+	return (CHAR8)caracter;
+}
+
+esp_ret_t at_esp_init(void)
+{
+	
+	if(esp_state != INIT)
+	{
+		return ESP_STATE_ERR;
+	}
+	
+	/* init ESP_UART */
+	uart_init(ESP_UART,ESP_BAUD,ESP_UART_BUFSIZE,ESP_MUTEX,ESP_MUTEX_PRIO);
+	
+	/* check init */
+	esp_acquire();	
+	esp_print("AT\r\n");	
+	at_esp_print_reply();
+	esp_release();
+	printSer(USE_USB,"\r\n");
+	
+	esp_acquire();	
+	esp_print("AT+CWMODE=3\r\n");
+	at_esp_print_reply();
+	esp_release();	
+	printSer(USE_USB,"\r\n");	
+	
+	esp_acquire();
+	esp_print(("AT+CWJAP=\"" ESP_AP "\",\"" ESP_PWD "\"\r\n"));
+	at_esp_print_reply();
+	esp_release();	
+	printSer(USE_USB,"\r\n");
+	
+	esp_state = CLOSE;
+	return ESP_OK;
+	
+}
+esp_ret_t at_esp_open(void)
+{
+	if(esp_state != CLOSE)
+	{
+		return ESP_STATE_ERR;
+	}
+	
+	esp_acquire();	
+	esp_print("AT+CIPSTA?\r\n");
+	at_esp_print_reply();
+	esp_release();		
+	printSer(USE_USB,"\r\n");
+	
+	esp_acquire();	
+	esp_print("AT+CIPRESOLVE=\"emon-gpsnetcms.rhcloud.com\"\r\n");
+	at_esp_print_reply();
+	esp_release();		
+	printSer(USE_USB,"\r\n");
+	
+	esp_acquire();	
+	esp_print("AT+CIPCREATE=\"TCP\",10201,2048\r\n");	
+	at_esp_getchar();
+	esp_release();		
+	printSer(USE_USB,"\r\n");
+	
+	
+	esp_acquire();	
+	esp_print("AT+CIPCONNECT=0,\"54.160.189.224\",80\r\n");	
+	at_esp_print_reply();
+	esp_release();			
+	printSer(USE_USB,"\r\n");
+	
+	esp_state = OPEN;
+	
+	return ESP_OK;
+}
+
+esp_ret_t at_esp_send(INT8U* dados)
+{
+		
+	if(esp_state != OPEN)
+	{
+		return ESP_STATE_ERR;
+	}
+	
+	/* sending */	
+	esp_acquire();
+	
+#if 0	
+	#define SEND_STRING "AT+CIPSENDI=0,\"GET /input/post.json?json={p:3}&apikey=90a004390f3530d0ba10199ac2b1ac3d HTTP/1.1\\r\\nHost: emon-gpsnetcms.rhcloud.com\\r\\n\\r\\n\\r\\n\""
+	esp_print(SEND_STRING);
+	esp_print("\r\n");	
+#else	
+	if(dados != NULL)
+	{
+		esp_print(dados);	
+	}
+#endif	
+	
+	esp_print("\r\n");
+	at_esp_print_reply();
+	printSer(USE_USB,"\r\n");
+	
+	esp_release();	
+	
+	return ESP_OK;
+}
+
+esp_ret_t at_esp_receive(CHAR8* buff, INT8U* len)
+{
+
+	if(esp_state != OPEN)
+	{
+		return ESP_STATE_ERR;
+	}
+	
+	esp_acquire();	
+	esp_print("AT+CIPRD=0\r\n");	
+	*len = at_esp_get_reply(buff,*len);
+	//at_esp_print_reply();
+	esp_release();		
+	printSer(USE_USB,"\r\n");
+		
+	/* try to receive */	
+	return ESP_OK;
+}
+
+esp_ret_t at_esp_close(void)
+{
+	if(esp_state != OPEN)
+	{
+		return ESP_STATE_ERR;
+	}
+	
+	esp_acquire();	
+		esp_print("AT+CIPCLOSE=0\r\n");	
+		at_esp_print_reply();
+		esp_print("AT+CIPCLOSE=1\r\n");	
+		at_esp_print_reply();
+	esp_release();		
+	printSer(USE_USB,"\r\n");
+		
+	esp_state = CLOSE;
+	return ESP_OK;
+}
+
+
---
+++ brtos_unet/src/dev/esp8266/esp8266_at.h
@@ -0,0 +1,177 @@
+/* The License
+ * 
+ * Copyright (c) 2015 Universidade Federal de Santa Maria
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+
+*/
+/*
+ * esp8266_at.h
+ *
+ *  Created on: Mar 17, 2015
+ *      Author: Carlos H. Barriquello
+ */
+ 
+ 
+/** \addtogroup devices
+ *  @{
+ */
+ 
+ 
+ /** \addtogroup modems
+ *  @{
+ */
+ 
+ 
+ /*! \defgroup esp8266 Modem ESP8266 
+ * @{
+ Funções para modem ESP8266 
+ */
+ 
+ /*! \file esp8266_at.h
+ \brief Interface para ler/escrever dados no modem ESP8266.
+ 
+ Funcoes de leitura e escrita sao feitas por UART.
+ */
+
+#ifndef ESP8266_AT_H_
+#define ESP8266_AT_H_
+
+#include "AppConfig.h"
+#include "OS_types.h"
+#include "UART.h"
+
+#if COLDUINO
+#include "virtual_com.h"
+#endif
+#include "terminal_io.h"
+
+// configuration
+#if COLDUINO
+#define ESP_ENABLE 1
+#else
+#define ESP_ENABLE 0
+#endif
+
+#define ESP_BAUD 9600
+#define ESP_UART 2
+#define ESP_TCP_PORT		80
+#define ESP_TCP_LOCAL_PORT	10201
+#define ESP_TCP_CTX_NUM		0
+#define ESP_TCP_CTX_SIZE	2048
+//#define ESP_AP				"1209B"
+//#define ESP_PWD				"eletronica"
+
+#define ESP_AP				"GISELE_e_CARLOS"
+#define ESP_PWD				"01122007"
+
+#define ESP_UART_BUFSIZE	64
+#define ESP_UART_TIMEOUT	2000
+
+#if ESP_ENABLE
+
+/* define esp uart functions*/
+#if ESP_UART == 1 
+	#if ENABLE_UART1 == FALSE
+		#error "UART 1 is disabled"
+	#endif
+	#define	ESP_UART_PINS 		UART1_PTA1_PTA2
+	#define ESP_UART_MUTEX		UART1_MUTEX
+	#define esp_print(x)		printSer(USE_UART1,x);
+	#define esp_putchar(x)		putcharSer(USE_UART1,x)
+	extern BRTOS_Queue 			*Serial1;
+	#define ESP_QUEUE			Serial1
+	#if ESP_UART_MUTEX
+		#define ESP_MUTEX		TRUE
+		#define ESP_MUTEX_PRIO	UART1_MUTEX_PRIO
+		#define esp_acquire()	uart1_acquire()
+		#define esp_release()	uart1_release()		
+	#else
+		#define ESP_MUTEX			FALSE
+		#define ESP_MUTEX_PRIO		0
+		#define esp_acquire()		
+		#define esp_release()	
+	#endif
+#elif ESP_UART == 2
+	#if ENABLE_UART2 == FALSE
+		#error "UART 2 is disabled"
+	#endif	
+	#define ESP_UART_MUTEX		UART2_MUTEX
+	#define esp_print(x)		printSer(USE_UART2,x)
+	#define esp_putchar(x)		putcharSer(USE_UART2,x)
+	extern BRTOS_Queue 			*Serial2;
+	#define ESP_QUEUE			Serial2
+	#if ESP_UART_MUTEX
+		#define ESP_MUTEX		TRUE
+		#define ESP_MUTEX_PRIO	UART2_MUTEX_PRIO
+		#define esp_acquire()	uart2_acquire()
+		#define esp_release()	uart2_release()
+	#else
+		#define ESP_MUTEX			FALSE
+		#define ESP_MUTEX_PRIO		0
+		#define esp_acquire()		
+		#define esp_release()	
+	#endif
+#else
+	#error "UART port is not specified"
+#endif
+#endif
+
+#include "modem.h"
+
+typedef state_t	esp_state_t;
+
+typedef enum 
+{
+	ESP_OK,
+	ESP_STATE_ERR,
+	ESP_APCONN_ERR,
+	ESP_TCPCONN_ERR
+}esp_ret_t;
+
+/* example
+ * AT+CWLAP
+ * AT+CWJAP="1209B","eletronica"
+ * AT+CIPSTA?
+ * AT+CIPSTAMAC?
+ * AT+CIPCREATE="TCP",10201,2048
+ * AT+CIPCONNECT=0,"54.160.189.224",80
+ * AT+CIPSENDI=0,"GET /input/post.json?json={power:150}&API_KEY HTTP/1.1\r\nHost: emon-gpsnetcms.rhcloud.com\r\n\r\n"
+ * AT+CIPRD=0
+ * AT+CIPCLOSE=0
+ *  
+ */
+
+esp_ret_t at_esp_init(void);
+esp_ret_t at_esp_open(void);
+esp_ret_t at_esp_send(INT8U* dados);
+esp_ret_t at_esp_receive(CHAR8* buff, INT8U* len);
+esp_ret_t at_esp_close(void);
+CHAR8 at_esp_getchar(void);
+
+
+INT8U esp_set_hostname(CHAR8 *host);
+INT8U esp_get_ip(void);
+INT8U esp_set_ip(CHAR8 * _ip);
+
+
+#endif /* ESP8266_AT_H_ */
+/** @} */
+/** @} */
+/** @} */
